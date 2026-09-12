c93dc1501bf6e8c1fd330442ce1f0ecc8d8a1648
Gohulrahesh <gokurahesh@gmail.com>
2026-09-12 23:16:03 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ def execute_command(c):
     for d in os.get_exec_path():
         if os.access(fullpath:=os.path.join(d,c),os.X_OK):
             return fullpath
+def builtin_commands(c):
+    return c in {"echo","exit","type"}
 def main():
     while True:
         sys.stdout.write("$ ")
@@ -14,18 +16,18 @@ def main():
         cmd=parts[0]
         args=parts[1:] if len(parts)>1 else ""
 
-
-        if command=="exit":
-            break
-        elif command.startswith("echo"):
-            print(command[5:])
-        elif command.startswith("type"):
-            if cmd in ["echo","type","exit"]:
-                print(f"{cmd} is a shell builtin")
-            elif path:= shutil.which(cmd):
-                print(f"{cmd} is {path}")
-            else:
-                print(f"{cmd} not found")
+        if builtin_commands(cmd):
+            if command=="exit":
+                break
+            elif command.startswith("echo"):
+                print(command[5:])
+            elif command.startswith("type"):
+                if cmd in ["echo","type","exit"]:
+                    print(f"{cmd} is a shell builtin")
+                elif path:= shutil.which(cmd):
+                    print(f"{cmd} is {path}")
+                else:
+                    print(f"{cmd} not found")
         elif execute_command(cmd):
             subprocess.run(parts)
         else:
