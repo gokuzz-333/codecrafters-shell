40cd61657227afe2956888b97a5b5280a8632cbd
Gohulrahesh <gokurahesh@gmail.com>
2026-09-12 22:51:32 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,15 +5,18 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command=input()
+        cmd=command[5:]
         if command=="exit":
             break
         elif command.startswith("echo"):
             print(command[5:])
         elif command.startswith("type"):
-            if command[5:] in ["echo","type","exit"]:
-                print(f"{command[5:]} is a shell builtin")
+            if cmd[5:] in ["echo","type","exit"]:
+                print(f"{cmd[5:]} is a shell builtin")
+            elif path:= shutil.which(cmd):
+                print(f"{cmd} is {path}")
             else:
-                print(f"{command[5:]} not found")
+                print(f"{cmd} not found")
         else:
             print(f"{command}: command not found")
 
