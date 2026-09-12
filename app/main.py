d8c8af9f0c86a980c809b7c40a4bee2325fcd36e
Gohulrahesh <gokurahesh@gmail.com>
2026-09-12 22:25:04 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,6 +7,8 @@ def main():
         command=input()
         if command=="exit":
             break
+        if command.startswith("echo"):
+            print(command[5:])
         print(f"{command}: command not found")
 
 
