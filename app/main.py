565cd1fc6be9f318d20beebc4432f2fbaf60e7d6
Gohulrahesh <gokurahesh@gmail.com>
2026-09-13 09:41:31 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -24,25 +24,31 @@ def main():
         if builtin_commands(cmd):
             if cmd=="exit":
                 break
+
             elif cmd=="echo":
                 print(f"{' '.join(args)}")
+
             elif cmd=="pwd":
                 print(os.getcwd())
+
             elif cmd=="cd":
-                os.chdir(args[0])
-            elif cmd=="type":
                 try:
                     os.chdir(args[0])
                 except FileNotFoundError:
                     print(f"cd: <{args[0]}>: No such file or directory")
+
+            elif cmd=="type":
+               
                 if builtin_commands(args[0]):
                     print(f"{args[0]} is a shell builtin")
                 elif full_path:=execute_command(args[0]):
                     print(f"{args[0]} is {full_path}")
                 else:
                     print(f"{args[0]}: not found")
+
         elif execute_command(cmd):
             subprocess.run(parts)
+            
         else:
             print(f"{command}: command not found")
 
